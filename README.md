67fd76bc27bd4e38ab543090d243774c0fff67cf
Domingo Gonzales III <34903375+DomGonzalesIII@users.noreply.github.com>
2020-06-16 14:13:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LDTemplateFiles
+These are a set of templates for setting up a learning den page. They serve to save time and maintain consistency.
---
+++ index.js
@@ -0,0 +1,5 @@
+// jshint esversion: 8
+
+/* Copyright (c) 2020 The Learning Den */
+
+alert('Click on the thing or press space to change the thing');
